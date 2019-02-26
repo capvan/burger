9869b628a7db325bc73204bb81eb1e30e9504f15
Sergey Krivenko <vanseg@mail.ru>
2019-02-26 03:44:00 +0300

    Добавлен JS для горизонтального аккордеона

--- source/js/main.js
+++ source/js/main.js
@@ -41,7 +41,7 @@ let menu = (function (options) {
 menu.openMenu();
 
 
-//Горизонтальный слайдер
+//Вертикальный аккордеон
 let teamAccoJS = () => {
     //выборка всех ссылок
     let oTeamLink = document.querySelectorAll(".team-accordion__link");
@@ -61,7 +61,8 @@ let teamAccoJS = () => {
                 teamAccordeonDetails.style.height = "0px";
                 activePerson.classList.remove("is_active");
             }
-//Если в activePerson undefined(false). Отрицание = кликов нет, выполнится код
+//Если в activePerson undefined(false). Отрицание = кликов нет, выполнится код. Если активный класс уже есть на странице
+// и он не равен querySelector, activePErson станет true
             if(!activePerson || activePerson.querySelector(".team-accordion__link") != e.target){
                 //Находим e.target и добавляем активный класс
                 let currentPerson = e.target.closest(".team-accordion__item");
@@ -77,3 +78,55 @@ let teamAccoJS = () => {
 };
  teamAccoJS();
 
+
+//Горизонтальный аккордеон
+
+let verticalAcco3 = () => {
+    
+    let calculateWidth = () => {
+        //определение ширины экрана
+        let windowWidth = window.innerWidth; //ширина всего окна
+
+        let links = document.querySelectorAll(".menu-accordion__link");
+        let link = document.querySelector(".menu-accordion__link");
+        //parseFloat - превращение значения ширины link в чичло с плавающей точкой
+        let linksWidth = parseFloat(getComputedStyle(link).width);
+
+        //вернет тернарный оператор. Если reqWidth - истина, то запишется условие 1 
+        let reqWidth = windowWidth - linksWidth * links.length;
+        return reqWidth > 550 ? 550 : reqWidth;
+    };
+
+    let oTeamLink = document.querySelectorAll(".menu-accordion__link");
+
+        oTeamLink.forEach(function(personName) {
+            personName.addEventListener("click", function(e) {
+                e.preventDefault();
+                let activePerson = document.querySelector(".menu-accordion__item.is_active");
+
+                if(activePerson) {
+                    let teamAccordeonDetails = activePerson.querySelector(".menu-accordion__content");
+                    teamAccordeonDetails.style.width = "0px";
+                    activePerson.classList.remove("is_active");
+                }
+
+                if(!activePerson || activePerson.querySelector(".menu-accordion__link") != this) {
+                    let currentPerson = this.closest(".menu-accordion__item");
+
+                    currentPerson.classList.add("is_active");
+                    let currentPersonInfo = currentPerson.querySelector(".menu-accordion__content");
+                    currentPersonInfo.style.width = calculateWidth() + 'px';
+                }
+            })
+        })
+        // //Обработчик клика вне аккордеона
+        // document.addEventListener("click", e => {
+        //     //Запись активной ссыдки
+        //     let activePerson = document.querySelector(".menu-accordion__item.is_active");
+        //     const target = e.target;
+        //     if (!target.closest(".menu-accordion")) {
+        //         closeItem(activePerson);
+        //     }
+        // });
+    };
+verticalAcco3();
